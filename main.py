71921fcac32eccd0b590ead89382609185362792
niranjanrajesh02 <niranjanrajesh02@gmail.com>
2023-07-27 02:26:21 +0700

    added logging

--- main.py
+++ main.py
@@ -6,35 +6,43 @@ from dataset import get_split_data
 import seaborn as sns
 from sklearn.preprocessing import LabelBinarizer
 from utils import make_confusion_matrix, make_lineplot
+import sys
 
-OVERLAP = False
 MAX_GROUP_TEST = 7
 accuracies = []
 
-for i in range(2, MAX_GROUP_TEST + 1):
-    
-    print(f'Testing with {i} functional groups ... \n')
-          
-    X_train, X_test, y_train, y_test, classes = get_split_data(overlap=OVERLAP, n_fgroups=i)
+f = open('./Decision Tree/log.txt', 'w+')
+sys.stdout = f
 
-    clf = DecisionTreeClassifier()
+for o in [True, False]:
+    print(" ================================ \n")
+    print(f'Running with overlap = {o} ... \n')
+    OVERLAP = o
+    for i in range(2, MAX_GROUP_TEST + 1):
+        
+        print(f'Testing with {i} functional groups ... \n')
+            
+        X_train, X_test, y_train, y_test, classes = get_split_data(overlap=OVERLAP, n_fgroups=i)
 
-    print("Training Model ...\n")
-    clf.fit(X_train, y_train)
+        clf = DecisionTreeClassifier()
 
-    print("Testing Model ... \n")
-    y_pred = clf.predict(X_test)
-    accuracy = accuracy_score(y_test, y_pred)
-    accuracies.append(accuracy)
-    
-    print(f'Accuracy of the model is: {accuracy}\n')
+        print("Training Model ...\n")
+        clf.fit(X_train, y_train)
 
-    # convert one hot tensors to 1d labels
-    y_pred = np.argmax(y_pred, axis=1)
-    y_test = np.argmax(y_test, axis=1)
+        print("Testing Model ... \n")
+        y_pred = clf.predict(X_test)
+        accuracy = accuracy_score(y_test, y_pred)
+        accuracies.append(accuracy)
+        
+        print(f'Accuracy of the model is: {accuracy}\n')
 
-    make_confusion_matrix(y_test, y_pred, classes, accuracy=accuracy, overlap=OVERLAP, n_fgroups=i)
+        # convert one hot tensors to 1d labels
+        y_pred = np.argmax(y_pred, axis=1)
+        y_test = np.argmax(y_test, axis=1)
 
-make_lineplot(accs=accuracies,overlap=OVERLAP)
+        make_confusion_matrix(y_test, y_pred, classes, accuracy=accuracy, overlap=OVERLAP, n_fgroups=i)
+     
+    make_lineplot(accs=accuracies,overlap=OVERLAP)
 
 print("Done!")
+f.close()
